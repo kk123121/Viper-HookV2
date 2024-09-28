c9050deb824f23fb64d3cebca5c4c96c847c52a0
Simple IP <kaidenjclaflin@gmail.com>
2024-09-28 16:27:34 -0700

    Add files via upload

---
+++ Settings/Program/Config/Config.py
@@ -0,0 +1,17 @@
+
+
+name_tool = "Viper-Hook"
+type_tool = "Multi-Tools"
+version_tool = "6.0"
+coding_tool = "Python 3"
+language_tool = "EN"
+creator = "Simple"
+platform = "Windows 10/11 & Linux"
+discord_server = "None"
+website = "https://mrnugget.carrd.co/"
+github_tool = "github.com/kk123121"
+telegram = "sadly  no telegram"
+url_config = " "
+url_downloads = ""
+license = " MIT"
+
